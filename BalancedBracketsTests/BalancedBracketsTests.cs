2f270c01ff50b8975381a770365da4260ec81f54
Richard Carino <richard.carino17@gmail.com>
2020-06-10 05:13:38 +1000

    to turn in

--- BalancedBracketsTests/BalancedBracketsTests.cs
+++ BalancedBracketsTests/BalancedBracketsTests.cs
@@ -8,10 +8,100 @@ namespace BalancedBracketsTests
     {
         // TODO: Add tests to this file.
 
+        //[TestMethod]
+        //public void EmptyTest()
+        //{
+        //    Assert.AreEqual(true, true);
+        //}
+
         [TestMethod]
-        public void EmptyTest()
+        public void CheckIfBracketsAreInTheRightOrder()
         {
-            Assert.AreEqual(true, true);
+            string testString = "]test[";
+
+            Assert.AreEqual(false, BalancedBrackets.HasBalancedBrackets(testString));
         }
+
+        [TestMethod]
+        public void CheckIfEvenNumberofOpeningAndClosingBrackets()
+        {
+            string testString = "[testString][";
+
+            Assert.AreEqual(false, BalancedBrackets.HasBalancedBrackets(testString));
+        }
+
+        [TestMethod]
+        public void ReturnTrueIfStringIsEmpty()
+        {
+            string testString = "";
+            
+            Assert.AreEqual(true, BalancedBrackets.HasBalancedBrackets(testString));
+        }
+
+        [TestMethod]
+        public void ReturnsTrueRegardlessOfBracketPlacement()
+        {
+            string testString = "Test[String]";
+            Assert.AreEqual(true, BalancedBrackets.HasBalancedBrackets(testString));
+        }
+
+        [TestMethod]
+        public void ReturnsTrueIfWordwithNoBracket()
+        {
+            string testString = "Test";
+            Assert.AreEqual(true, BalancedBrackets.HasBalancedBrackets(testString));
+        }
+
+        [TestMethod]
+        public void AcceptsWhiteSpaceWithinBracket()
+        {
+            string testString = "[            ]";
+            Assert.AreEqual(true, BalancedBrackets.HasBalancedBrackets(testString));
+        }
+
+        [TestMethod]
+        public void AcceptsWhiteSpace()
+        {
+            string testString = "            ";
+            Assert.AreEqual(true, BalancedBrackets.HasBalancedBrackets(testString));
+        }
+
+        [TestMethod]
+        public void AcceptsSpecialChars()
+        {
+            string testString = "$%^[^&]";
+            Assert.AreEqual(true, BalancedBrackets.HasBalancedBrackets(testString));
+        }
+
+        [TestMethod]
+        public void AccountsForMultiplebracketPairs()
+        {
+            string testString = "Test[String][idv]hin";
+            Assert.AreEqual(true, BalancedBrackets.HasBalancedBrackets(testString));
+        }
+
+        [TestMethod]
+        public void RejectsUnorderedMultipleBracketpairs()
+        {
+            string testString = "test[]te]s[t";
+            Assert.AreEqual(false, BalancedBrackets.HasBalancedBrackets(testString));
+        }
+
+        [TestMethod]
+        public void AcceptsProperlyNestedBrckets()
+        {
+            string testString = "[[[[]]]]";
+            Assert.AreEqual(true, BalancedBrackets.HasBalancedBrackets(testString));
+        }
+
+        [TestMethod]
+        public void RejectsImproperlyNestedBrckets()
+        {
+            string testString = "[[[[[]]]]";
+            Assert.AreEqual(false, BalancedBrackets.HasBalancedBrackets(testString));
+        }
+
+
+
     }
 }
